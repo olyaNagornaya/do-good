4ec5c403d5c0e5fe42f3fe08c19e79e74845a570
Nagornaya <o.nagornaya7@gmail.com>
2022-03-17 14:38:38 +0300

    втягиваю dev

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import MainPage from "./pages/MainPage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import MyProductCard from "./components/MyProductCard/MyProductCard";
 
 function App() {
   return (
       <>
         <Navbar />
+          <MyProductCard/>
         <MainPage />
           <Footer />
       </>
