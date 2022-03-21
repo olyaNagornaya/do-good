61c48cdc31b18db2000e08e65b427120f3634c0a
Seregb <seregb68@gmail.com>
2022-03-21 11:51:41 +0300

    add API geocoding

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -26,11 +26,6 @@ const Form = () => {
     formData.append("file", file);
     setInputs({ ...inputs, img: formData });
   };
-  const inputAvatarHandler = (e) => {
-    const file = e.target.files[0];
-    console.log(file);
-    uploadHandler(file);
-  };
 
   const inputCheckBox = (e) => {
     // console.log(e.target.checked);
@@ -48,150 +43,163 @@ const Form = () => {
       });
     } else {
       e.preventDefault();
-      alert('Согласись с нашими условиями платформы!!!')
+      alert("Согласись с нашими условиями платформы!!!");
     }
+  }
     const inputAvatarHandler = (e) => {
-        const file = e.target.files[0];
-        console.log(file);
-        uploadHandler(file);
-    }
-
-
+      const file = e.target.files[0];
+      console.log(file);
+      uploadHandler(file);
+    };
 
     return (
-        <>
-            <main id="main"></main>
-            {/* <!-- main --> */}
+      <>
+        <main id="main"></main>
+        {/* <!-- main --> */}
 
-            <div className="main-w3layouts wrapper maindiv">
-                {/*<h1>{err}</h1>*/}
-                <h1>Добавление продукта</h1>
-                <div className="main-agileinfo">
-                    <div className="agileits-top formdesign">
-                        <form className="" >
-                            <input
-                                className="text inputformdecor"
-                                type="text"
-                                name="title"
-                                placeholder="Название"
-                                required=""
-                                value={inputs.title}
-                                onChange={handleChange}
-                            />
-                            <input className="inputphoto input-file" id="file" onChange={(e) => inputAvatarHandler(e)}
-                                   accept='image/*' type='file' name='img'/>
-                            <label htmlFor="file" className="btn btn-tertiary js-labelFile">
-                                {inputs.img ? <i className="bi bi-check2-square"></i> :
-                                    <i className="icon fa fa-check"></i>}
-                                <span
-                                    className="js-fileName"> {inputs.img ? " Фото загружено" : " Загрузить фото"}</span>
-                            </label>
-                            <select
-                                className="text email selectformdecor"
-                                placeholder="Категория"
-                                value={inputs.category}
-                                onChange={handleChange}
-                                name="category"
-                            >
-                                <option default>Выберите категорию</option>
-                                <option value="Food">Продукты</option>
-                                <option value="Clothers">Одежда</option>
-                                <option value="Furniture">Мебель</option>
-
-                            </select>
-
-                            <input
-                                className="text email inputformdecor"
-                                type="text"
-                                name="description"
-                                placeholder="Описание"
-                                required=""
-                                value={inputs.description}
-                                onChange={handleChange}
-                            />
-                            <input
-                                className="text email inputformdecor"
-                                type="text"
-                                name="geolocation"
-                                placeholder="Город"
-                                required=""
-                                value={inputs.geolocation}
-                                onChange={handleChange}
-                            />
-                            <input
-                                className="text email inputformdecor"
-                                type="text"
-                                name="city"
-                                placeholder="Адрес"
-                                required=""
-                                value={inputs.city}
-                                onChange={handleChange}
-                            />
-
-                            <input
-                                className="text email inputformdecor"
-                                type="date"
-                                name="validUntil"
-                                placeholder="Действительно до"
-                                required=""
-                                value={inputs.validUntil}
-                                onChange={handleChange}
-                            />
-
-<div className="wthree-text">
-                <label className="anim">
-                  <input
-                    name="checkBox"
-                    type="checkbox"
-                    className="checkbox"
-                    required=""
-                    onChange={inputCheckBox}
-                  />
-                  <span className="textoncheckbox">
-                    Я согласен с условиями платформы!
+        <div className="main-w3layouts wrapper maindiv">
+          {/*<h1>{err}</h1>*/}
+          <h1>Добавление продукта</h1>
+          <div className="main-agileinfo">
+            <div className="agileits-top formdesign">
+              <form className="">
+                <input
+                  className="text inputformdecor"
+                  type="text"
+                  name="title"
+                  placeholder="Название"
+                  required=""
+                  value={inputs.title}
+                  onChange={handleChange}
+                />
+                <input
+                  className="inputphoto input-file"
+                  id="file"
+                  onChange={(e) => inputAvatarHandler(e)}
+                  accept="image/*"
+                  type="file"
+                  name="img"
+                />
+                <label htmlFor="file" className="btn btn-tertiary js-labelFile">
+                  {inputs.img ? (
+                    <i className="bi bi-check2-square"></i>
+                  ) : (
+                    <i className="icon fa fa-check"></i>
+                  )}
+                  <span className="js-fileName">
+                    {" "}
+                    {inputs.img ? " Фото загружено" : " Загрузить фото"}
                   </span>
                 </label>
-                <div className="clear"> </div>
-              </div>
-              <button className="btnlogin" type="submit" onClick={addProdToDB}>
-                ДОБАВИТЬ
-              </button>
-            </form>
-            <p>
-              Вы передумали? <Link to="/profile"> Выйти</Link>
-            </p>
+                <select
+                  className="text email selectformdecor"
+                  placeholder="Категория"
+                  value={inputs.category}
+                  onChange={handleChange}
+                  name="category"
+                >
+                  <option default>Выберите категорию</option>
+                  <option value="Food">Продукты</option>
+                  <option value="Clothers">Одежда</option>
+                  <option value="Furniture">Мебель</option>
+                </select>
+
+                <input
+                  className="text email inputformdecor"
+                  type="text"
+                  name="description"
+                  placeholder="Описание"
+                  required=""
+                  value={inputs.description}
+                  onChange={handleChange}
+                />
+                <input
+                  className="text email inputformdecor"
+                  type="text"
+                  name="geolocation"
+                  placeholder="Город"
+                  required=""
+                  value={inputs.geolocation}
+                  onChange={handleChange}
+                />
+                <input
+                  className="text email inputformdecor"
+                  type="text"
+                  name="city"
+                  placeholder="Адрес"
+                  required=""
+                  value={inputs.city}
+                  onChange={handleChange}
+                />
+
+                <input
+                  className="text email inputformdecor"
+                  type="date"
+                  name="validUntil"
+                  placeholder="Действительно до"
+                  required=""
+                  value={inputs.validUntil}
+                  onChange={handleChange}
+                />
+
+                <div className="wthree-text">
+                  <label className="anim">
+                    <input
+                      name="checkBox"
+                      type="checkbox"
+                      className="checkbox"
+                      required=""
+                      onChange={inputCheckBox}
+                    />
+                    <span className="textoncheckbox">
+                      Я согласен с условиями платформы!
+                    </span>
+                  </label>
+                  <div className="clear"> </div>
+                </div>
+                <button
+                  className="btnlogin"
+                  type="submit"
+                  onClick={addProdToDB}
+                >
+                  ДОБАВИТЬ
+                </button>
+              </form>
+              <p>
+                Вы передумали? <Link to="/profile"> Выйти</Link>
+              </p>
+            </div>
           </div>
+          <ul className="colorlib-bubbles">
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+            <li>
+              <img className="img-bubbles" src="" />
+            </li>
+          </ul>
         </div>
-        <ul className="colorlib-bubbles">
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-          <li>
-            <img className="img-bubbles" src="" />
-          </li>
-        </ul>
-      </div>
-      {/* <!-- //main --> */}
-    </>
-  );
-};
+        {/* <!-- //main --> */}
+      </>
+    );
+  };
 
 export default Form;
--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -5,26 +5,41 @@ import {
   Map,
   Placemark,
   GeolocationControl,
-  Clusterer
+  Clusterer,
 } from "react-yandex-maps";
+import DB from "./points.js";
 
 const mapState = {
   center: [55.751574, 37.573856],
-  zoom: 9
+  zoom: 9,
 };
+
+
 
 export function App() {
   const [cluster, setCluster] = useState(null);
+  const [address, setAddress] = useState([]);
+
+  //Москва, улица Новый Арбат, дом 24
+
+  // fetch(
+  //   `https://nominatim.openstreetmap.org/search?q=24+Новый+Арбат%2C+Москва&format=geojson`
+  // )
+  //   .then((resp) => {
+  //     return resp.json();
+  //   })
+  //   .then((data) => console.log(data));
+
   return (
-    <div id='map' className="App">
+    <div id="map" className="App">
       <YMaps>
         <Map
           defaultState={mapState}
           width="100%"
           height="100%"
-          instanceRef={ref => {
+          instanceRef={(ref) => {
             if (ref) {
-              ref.events.add("click", e => {
+              ref.events.add("click", (e) => {
                 ref.balloon.close();
               });
             }
@@ -34,44 +49,25 @@ export function App() {
           <Clusterer
             modules={["clusterer.addon.balloon"]}
             options={{}}
-            instanceRef={ref => {
+            instanceRef={(ref) => {
               if (ref) {
                 setCluster(ref);
               }
             }}
           >
-            <Placemark
-              modules={["geoObject.addon.balloon"]}
-              geometry={[55.684758, 37.738521]}
-              properties={{
-                balloonContentHeader: "Balloon1",
-                balloonContent: "Balloon1 <strong>Test</strong>"
-              }}
-            />
-            <Placemark
-              modules={["geoObject.addon.balloon"]}
-              geometry={[55.8, 37.8]}
-              properties={{
-                balloonContentHeader: "Balloon2",
-                balloonContent: "Balloon2 <strong>Test</strong>"
-              }}
-            />
-            <Placemark
-              modules={["geoObject.addon.balloon"]}
-              geometry={[55.5, 37.738521]}
-              properties={{
-                balloonContentHeader: "Balloon3",
-                balloonContent: "Balloon3 <strong>Test</strong>"
-              }}
-            />
-            <Placemark
-              modules={["geoObject.addon.balloon"]}
-              geometry={[55.684758, 37]}
-              properties={{
-                balloonContentHeader: "Balloon4",
-                balloonContent: "Balloon4 <strong>Test</strong>"
-              }}
-            />
+            {DB.map((el) => {
+              return (
+                <Placemark
+                  key={el.id}
+                  modules={["geoObject.addon.balloon"]}
+                  geometry={el.coords}
+                  properties={{
+                    balloonContentHeader: el.title,
+                    balloonContent: el.description,
+                  }}
+                />
+              );
+            })}
           </Clusterer>
         </Map>
       </YMaps>
@@ -81,5 +77,3 @@ export function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-
