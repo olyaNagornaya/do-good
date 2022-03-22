925074af35d135392eb93045b166b9836289232d
Seregb <seregb68@gmail.com>
2022-03-22 12:12:51 +0300

    Added placemark on page prod

--- client/src/components/MapMini/index.js
+++ client/src/components/MapMini/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import React from "react";
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 function MapMini({ coordinate }) {
+  const DBO = useSelector((store) => store.post);
+  const id = useParams();
+  const ourPost = DBO.filter((el) => el.id === Number(id.id))
+  const ourCoord = [ourPost[0].coordinatesX, ourPost[0].coordinatesY]
+  // console.log(DBO, id, ourPost[0].coordinatesX);
 
-    let coordinateFromdb = coordinate?.split(' ').map(el => Number(el)).reverse()
-    const coordinates = [coordinateFromdb];
-
-    return (
-        <>
-            <YMaps>
-                <div>
-                    <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} width="100%" >
-                        {coordinates.map(coordinate => <Placemark key={coordinate} geometry={coordinate}  />)}
-                    </Map>
-                </div>
-            </YMaps>
-
-        </>
-    )
+  return (
+    <>
+      <YMaps>
+        <div>
+          <Map defaultState={{ center: [55.75, 37.57], zoom: 9 }} width="100%">
+            <Placemark key={coordinate} geometry={ourCoord} />
+          </Map>
+        </div>
+      </YMaps>
+    </>
+  );
 }
 
-export default MapMini
+export default MapMini;
